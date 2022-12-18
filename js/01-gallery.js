9dff578d67a4b311983f33523d89cbb4991cef06
Sylwia Florek <florekbuziaczek199166@gmail.com>
2022-12-18 13:16:38 +0100

    upload

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+
+galleryItems.forEach((image) => {
+  list.insertAdjacentHTML(
+    "beforeend",
+    `<div class="gallery__item">
+      <a class=gallery__link href="${image.original}">
+        <img src="${image.preview}" alt="${image.description}" class="gallery__image" data-source="${image.original}"/>
+      </a>
+    </div>`
+  );
+});
+
+list.addEventListener("click", imageModal);
+
+function imageModal(event) {
+  console.log(event.target);
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  event.preventDefault();
+  const instance = basicLightbox.create(`
+      <img src="${event.target.dataset.source}" width="800" height="600">
+      `);
+
+  instance.show();
+
+
+  document.addEventListener("keydown", function escapeKey(event) {
+    if (event.key === "Escape") {
+      instance.close();
+      document.removeEventListener("keydown", escapeKey);
+      console.log("esc");
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+
+galleryItems.forEach((image) => {
+  list.insertAdjacentHTML(
+    "beforeend",
+    `<li class="gallery__item">
+      <a class="gallery__link" href="${image.original}">
+        <img src="${image.preview}" alt="${image.description}" class="gallery__image"/>
+      </a>
+    </li>`
+  );
+});
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
